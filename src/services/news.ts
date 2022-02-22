b84e8d94f1db59229bb3dc8da9cec29240724dfc
wanglixiang <xsmarter@outlook.com>
2022-02-22 09:33:53 +0800

    chore: 更换接口http协议

--- src/services/news.ts
+++ src/services/news.ts
@@ -1,7 +1,10 @@
 import { request } from 'umi';
 
 export async function getNews() {
-  return request<any>('http://api.tianapi.com/topnews/index?key=b342d31a31eaf732ec37830ed2c290f1', {
-    method: 'get',
-  });
+  return request<any>(
+    'https://api.tianapi.com/topnews/index?key=b342d31a31eaf732ec37830ed2c290f1',
+    {
+      method: 'get',
+    },
+  );
 }
